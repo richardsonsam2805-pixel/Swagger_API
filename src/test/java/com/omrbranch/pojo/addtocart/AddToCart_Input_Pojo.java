c76e794bdc5c9d8088e22c8fc8a2b914a0e32262
richardsonsam2805 <richardsonsam2805@gmail.com>
2026-06-03 23:02:21 +0530

    new method

--- src/test/java/com/omrbranch/pojo/addtocart/AddToCart_Input_Pojo.java
+++ src/test/java/com/omrbranch/pojo/addtocart/AddToCart_Input_Pojo.java
@@ -11,5 +11,31 @@ public class AddToCart_Input_Pojo {
 	 public String product_id;
 	    public String product_variation_id;
 	    public String type;
+	    
+	    public void tester1() {
+			System.out.println("1");
+		}
+
+		public void tester12() {
+			System.out.println("2");
+		}
+
+		public void tester13() {
+			System.out.println("3");
+		}
+
+		public void tester14() {
+			System.out.println("4");
+		}
+
+		public void tester15() {
+			System.out.println("5");
+		}
+		
+		public void tester16() {
+			System.out.println("6");
+		}
 
 }
+
+
